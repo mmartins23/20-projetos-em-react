5d3b7bf96f04299114ebb4ea5ad3f1d5739f4b86
Marlon Martins <marlon-martins@outlook.com>
2024-09-09 22:53:32 +0200

    2: Projeto Cronometro

--- 2_cronômetro/react-cronometro-app/src/components/TimerControls.jsx
+++ 2_cronômetro/react-cronometro-app/src/components/TimerControls.jsx
@@ -2,9 +2,9 @@ const TimerControls = ({ timerOn, onStart, onStop, onReset, onLap }) => {
   return (
     <div className="timer-controls">
       {!timerOn && <button onClick={onStart}>Start</button>}
-      {timerOn && <button onClick={onStop}>Reset</button>}
+      {timerOn && <button onClick={onStop}>Stop</button>}
       {timerOn && <button onClick={onLap}>Lap</button>}
-      <button onClick={onReset}>Zerar</button>
+      <button onClick={onReset}>Reset</button>
     </div>
   )
 };
